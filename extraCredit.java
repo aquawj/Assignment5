ce8d006a2db4b2278d68b711e676d2bfa4de1e42
JaneWJ <wang.jia1@husky.neu.edu>
2017-02-21 12:46:22 -0800

    Update extraCredit.java

--- extraCredit.java
+++ extraCredit.java
@@ -2,26 +2,23 @@ package assignment5;
 
 public class extraCredit {
 	
-	public void merge(int[] nums1, int m, int[] nums2, int n) {
-		nums1=new int[m+n];
-		nums2=new int[n];
-		
-		int i,j,k;
-		for(i=0;i<m;i++){
-			for(j=0;j<n;j++){
-				if(nums2[j]<=nums1[i]){
-					for(k=i;k<m;k++){
-						nums1[k+1]=nums1[k];
-					}
-					nums1[i]=nums2[j];
-					for(k=j;k<n-1;k++){
-						nums2[k]=nums2[k+1];
-					}
-				}
-				else break;
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+		int i = 0, j = 0;
+
+		int countInNums1 = m;
+		for (j = 0; j < n; j++) {
+			while (nums1[i] < nums2[j] && i < nums1.length - 1) {
+				i++;
 			}
+			// found one in nums1 with the index of i that is bigger than nums2[j]
+			// move all the elements after nums1[i] backward
+			for (int k = countInNums1; k > i; k--) {
+				nums1[k] = nums1[k - 1];
+			}
+			nums1[i] = nums2[j]; // insert nums2[j] in this proper position
+			countInNums1++; // valid elements in nums1 added
+			i++;
 		}
-		
 	}
 	public static void main(String args[]){
 		extraCredit ec=new extraCredit();
@@ -33,7 +30,7 @@ public class extraCredit {
 		nm1[4]=6;
 
 		int[] nm2=new int[]{0,3,4,6,8};
-		ec.merge(nm1, 5, nm2, 5);
+		merge(nm1, 5, nm2, 5);
 		System.out.println("Merging two arrays: ");
 		for(int i=0;i<10;i++)
 			System.out.print(nm1[i]+" ");
